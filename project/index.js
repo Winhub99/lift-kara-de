775d409e0f2e83e450718180757bd4367ed98162
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-08-31 18:15:10 +0530

    created floors

--- project/index.js
+++ project/index.js
@@ -11,15 +11,16 @@ formEl.addEventListener('submit',function(e){
 
 
     for(let i=0;i<floors;i++){
+        const div = document.createElement('div')
         const hr = document.createElement('hr')
+        const span = document.createElement('span')
         hr.style.border ='2px solid black'
-        hr.style.width ='750px'
-        containerEL.appendChild(hr)
-    }
+        div.style.width ='750px'
+        div.style.marginBottom='125px'
 
-
-
-
-
-    
+        span.textContent= 'Floor ' + (floors - i)
+        div.appendChild(span)
+        div.appendChild(hr)
+        containerEL.appendChild(div)
+    }  
 })
